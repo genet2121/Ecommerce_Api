b35bab3e042581faba48fc807b3bf16a2e3f4854
muayyed <muayyed.siraj@gmail.com>
2024-06-21 16:17:49 +0300

    db tables update for admin and category filter

--- routes/tableNamesRoutes.js
+++ routes/tableNamesRoutes.js
@@ -4,12 +4,12 @@ const auth = require('../infrastructure/service/authentatication/auth');
 const Roles = require("../configration/enum");
 const router = express.Router();
 const tableNamesController = require('../controllers/tableNamesController');
-const { tableNameValidationRules, tableNameUpdateValidationRules, validate } = require('../validators/tableNamesValidator');
+const { tableNamesValidationRules, tableNamesUpdateValidationRules, validate } = require('../validators/tableNamesValidator');
 
 router.get('/', auth.authorize([Roles.ADMIN, Roles.BUYER, Roles.SELLER]), tableNamesController.getAllTableNames);
 router.get('/:id', auth.authorize([Roles.ADMIN, Roles.BUYER, Roles.SELLER]), tableNamesController.getTableNameById);
-router.post('/', auth.authorize([Roles.ADMIN, Roles.SELLER]), tableNameValidationRules, validate, tableNamesController.createTableName);
-router.put('/:id', auth.authorize([Roles.ADMIN, Roles.SELLER]), tableNameUpdateValidationRules, validate, tableNamesController.updateTableName);
+router.post('/', auth.authorize([Roles.ADMIN, Roles.SELLER]), tableNamesValidationRules, validate, tableNamesController.createTableName);
+router.put('/:id', auth.authorize([Roles.ADMIN, Roles.SELLER]), tableNamesUpdateValidationRules, validate, tableNamesController.updateTableName);
 router.delete('/:id', auth.authorize([Roles.ADMIN, Roles.BUYER, Roles.SELLER]), tableNamesController.deleteTableName);
 
 module.exports = router;
